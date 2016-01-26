3e6847fbbd83796185b2f91084f90f3526937d19
Shawydu <dxydyl2@gmail.com>
2016-01-26 18:56:30 +0000

    Add sticky footer, devise for user authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # For user automation email sending by deploying app
+  config.action_mailer.default_url_options = { host: 'flixter-shawydu.herokuapp.com' }
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
