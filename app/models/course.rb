44d823e629af45d5a2cdb0dcfa645addebb3f968
Shawydu <dxydyl2@gmail.com>
2016-01-27 04:15:52 +0000

    Add course page, validation for creating new course, course controller for students

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,7 @@
 class Course < ActiveRecord::Base
 	belongs_to :user
+
+	validates :title, :presence => {:message => "Title is straightforward for students"}
+	validates :description, :presence => {:message => "Let students get quick knowledge about this course"}
+	validates :cost, numericality: {greater_than_or_equal_to: 0, :message => "negative is a horrible story"}
 end
