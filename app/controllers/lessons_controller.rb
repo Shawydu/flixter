2e49ddc0098a191448585bd5558974cd5dd1a855
Shawydu <dxydyl2@gmail.com>
2016-01-30 14:08:41 +0000

    A little notice

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,17 @@
 class LessonsController < ApplicationController
+	before_action :require_enroll_to_view_lesson
 	def show
 		
 	end
 
 	private
+
+	def require_enroll_to_view_lesson
+		@current_course = current_lesson.section.course
+		if !@current_course.user.enrolled_in?(@current_course)
+			redirect_to course_path(@current_course), alert: 'You Must Enroll as Beginning'
+		end
+	end
 
 	helper_method :current_lesson
 	def current_lesson
